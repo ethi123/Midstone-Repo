db7155c382aa6da2dea4e02a6a19e04ae762ddb7
Your Name <pat.grygoruk@hotmail.com>
2020-10-20 15:57:23 -0400

    Working Window
    Window
    Messaging
    Exceptions

---
+++ Exception.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <exception>
+#include <string>
+
+class hException : public std::exception
+{
+public:
+	hException(int line, const char* file) noexcept;
+	const char* what() const noexcept override;
+	virtual const char* GetType() const noexcept;
+	int GetLine() const noexcept;
+	const std::string& GetFile() const noexcept;
+	std::string GetOriginString() const noexcept;
+private:
+	int line;
+	std::string file;
+protected:
+	mutable std::string whatBuffer;
+};
+
+//Cant throw exceptions in header because it is loaded first, before exceptions
