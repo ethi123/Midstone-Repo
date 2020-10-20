84d1d10a7b1e4331d6ab0ed5cfb2659f84c2ea4e
Your Name <pat.grygoruk@hotmail.com>
2020-10-20 17:05:54 -0400

    Working Windows

---
+++ Engine/Window.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "WCSM.h"
+#include "Exception.h"
+#include "WindowsMessageMap.h"
+#include <optional>
+#include <memory>
+
+
+class Window
+{
+public:
+	class Exception : public hException
+	{
+		using hException::hException;
+	public:
+		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+	private:
+		HRESULT hr;
+	};
+
+
+private:
+	class WindowClass
+	{
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const WindowClass&) = delete;
+		static constexpr const char* wndClassName = "Engine";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const char* name);
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+private:
+	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
+
+// error exception helper macro
+#define WND_EXCEPT(hr) Window::HrException(__LINE__,__FILE__,hr) //hResult
+#define WND_LAST_EXCEPT() Window::HrException(__LINE__,__FILE__,GetLastError()	) //some windows functions dont return error
